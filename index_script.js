e72c65f55f5bfa699cf3bfb00c98d0f4713ca987
Nadine <nadineperez96@gmail.com>
2018-02-21 17:27:28 -0500

    added songs to nav

--- index_script.js
+++ index_script.js
@@ -2,5 +2,6 @@
 
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='javascript_intro.html'>Javascript Introduction Assignment</a></li>"
+"<li><a href='javascript_intro.html'>Javascript Introduction Assignment</a></li>" +
+"<li><a href='songs.html'>Songs</a></li>"
 "</ul>";
