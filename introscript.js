389fc3bb3f0b25574ff506b12b57f8bda7cf7fbb
Nadine <nadineperez96@gmail.com>
2018-02-21 14:46:10 -0500

    Remove appendage

--- introscript.js
+++ introscript.js
@@ -10,12 +10,21 @@ button1.innerHTML = "Change color";
 // 2. Append somewhere
 var body = document.getElementsByTagName("body")[0];
 body.appendChild(button1);
-body.appendChild(button2);
+//body.appendChild(button2);
 
 // 3. Add event handlers
 button1.addEventListener ("click", function() {
   location.assign("http://nadineperez.github.io/");
 });
 
+/*
+button1.addEventListener ("click", function() {
+  changeColor();
+});
 
+function changeColor() {
+        document.getElementById('button2').style.color = 'red'
+        return false;
+} 
+*/
 
