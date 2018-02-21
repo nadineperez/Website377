df71b1b3ccb3ec5892d7c0f07eef3d93c600b0f2
Nadine <nadineperez96@gmail.com>
2018-02-21 17:27:58 -0500

    Update index_script.js

--- index_script.js
+++ index_script.js
@@ -2,6 +2,5 @@
 
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='javascript_intro.html'>Javascript Introduction Assignment</a></li>" +
-"<li><a href='songs.html'>Songs</a></li>"
+"<li><a href='javascript_intro.html'>Javascript Introduction Assignment</a></li>" 
 "</ul>";
