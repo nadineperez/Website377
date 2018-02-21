ab81523d404e817b8bf1e31a7252a4fb4f422880
Nadine <nadineperez96@gmail.com>
2018-02-21 14:52:35 -0500

    button1->button

--- introscript.js
+++ introscript.js
@@ -1,17 +1,17 @@
 // 1. Create buttons
-var button1 = document.createElement("button");
-button1.innerHTML = "Home";
+var button = document.createElement("button");
+button.innerHTML = "Home";
 
 //var button2 = document.createElement("button");
 //button1.innerHTML = "Change color";
 
 // 2. Append somewhere
 var body = document.getElementsByTagName("body")[0];
-body.appendChild(button1);
+body.appendChild(button);
 //body.appendChild(button2);
 
 // 3. Add event handlers
-button1.addEventListener ("click", function() {
+button.addEventListener ("click", function() {
   location.assign("http://nadineperez.github.io/");
 });
 
