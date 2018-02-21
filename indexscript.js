ee135c4ade0d6d2f5335d350c083209521da8e8e
Nadine <nadineperez96@gmail.com>
2018-02-21 14:10:41 -0500

    Create indexscript.js

---
+++ indexscript.js
@@ -0,0 +1,6 @@
+/*adding navigation menu*/
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='javascript_intro.html'>Javascript Introduction Assignment</a></li>"
+"</ul>";
