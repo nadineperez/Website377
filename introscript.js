0fe7860c72416f007accd399e74ef78f89771b43
Nadine <nadineperez96@gmail.com>
2018-02-21 15:17:28 -0500

    Update introscript.js

--- introscript.js
+++ introscript.js
@@ -8,7 +8,7 @@ var btn2 = document.createElement("button");
 btn2.innerHTML = "Change color";
 
 // 2. Append somewhere
-var body = document.getElementsByTagName("body");
+var body = document.getElementsByTagName("body")[0];
 body.appendChild(btn);
 body.appendChild(btn2);
 
