66618f3fe6ef32d89fcb78c3b194bac99f7909ef
Nadine <nadineperez96@gmail.com>
2018-02-21 14:57:41 -0500

    button-button2

--- introscript.js
+++ introscript.js
@@ -5,7 +5,7 @@ var button = document.createElement("button");
 button.innerHTML = "Home";
 
 var button2 = document.createElement("button");
-button.innerHTML = "Change color";
+button2.innerHTML = "Change color";
 
 // 2. Append somewhere
 var body = document.getElementsByTagName("body")[0];
