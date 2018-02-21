d2282a6a18185e7deaa7ccc7df69e3b699820d45
Nadine <nadineperez96@gmail.com>
2018-02-21 15:51:06 -0500

    Update introscript.js

--- introscript.js
+++ introscript.js
@@ -7,10 +7,14 @@ btn.innerHTML = "Home2";
 var btn2 = document.createElement("button");
 btn2.innerHTML = "Change color";
 
+var btn3 = document.createElement("button");
+btn3.innerHTML = "Display a message";
+
 // 2. Append somewhere
 var body = document.getElementsByTagName("body")[0];
 body.appendChild(btn);
 body.appendChild(btn2);
+body.appendChild(btn3);
 
 // 3. Add event handler
 btn.addEventListener ("click", function() {
