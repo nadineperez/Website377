54cc433804172fe49fdcfa7016b0cdb9d177ebdf
Nadine <nadineperez96@gmail.com>
2018-02-21 14:44:36 -0500

    change var names

--- introscript.js
+++ introscript.js
@@ -1,10 +1,11 @@
 /*adding navigation menu*/
 
 // 1. Create buttons
-var button = document.createElement("button1");
+var button1 = document.createElement("button1");
 button1.innerHTML = "Home";
 
-var button = document.createElement("button2");
+var button2 = document.createElement("button2");
+button1.innerHTML = "Change color";
 
 // 2. Append somewhere
 var body = document.getElementsByTagName("body")[0];
