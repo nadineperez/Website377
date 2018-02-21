90e793ad0bbc6e3cb6666eb6fdf2a24a51a048db
Nadine <nadineperez96@gmail.com>
2018-02-21 14:54:23 -0500

    revert

--- introscript.js
+++ introscript.js
@@ -1,28 +1,14 @@
-// 1. Create buttons
+/*adding navigation menu*/
+
+// 1. Create the button
 var button = document.createElement("button");
 button.innerHTML = "Home";
-
-//var button2 = document.createElement("button");
-//button1.innerHTML = "Change color";
 
 // 2. Append somewhere
 var body = document.getElementsByTagName("body")[0];
 body.appendChild(button);
-//body.appendChild(button2);
 
-// 3. Add event handlers
+// 3. Add event handler
 button.addEventListener ("click", function() {
   location.assign("http://nadineperez.github.io/");
 });
-
-/*
-button1.addEventListener ("click", function() {
-  changeColor();
-});
-
-function changeColor() {
-        document.getElementById('button2').style.color = 'red'
-        return false;
-} 
-*/
-
