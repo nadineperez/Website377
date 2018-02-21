071b370e3f4dbe65ce0ac5632f33c5edd6ff040f
Nadine <nadineperez96@gmail.com>
2018-02-21 14:56:28 -0500

    add button 2

--- introscript.js
+++ introscript.js
@@ -4,9 +4,13 @@
 var button = document.createElement("button");
 button.innerHTML = "Home";
 
+var button2 = document.createElement("button");
+button.innerHTML = "Change color";
+
 // 2. Append somewhere
 var body = document.getElementsByTagName("body")[0];
 body.appendChild(button);
+body.appendChild(button2);
 
 // 3. Add event handler
 button.addEventListener ("click", function() {
