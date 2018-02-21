aaa752ba894463353e08154832841b5f188c54b7
Nadine <nadineperez96@gmail.com>
2018-02-21 14:43:50 -0500

    Add second button

--- introscript.js
+++ introscript.js
@@ -1,17 +1,28 @@
 /*adding navigation menu*/
 
-// 1. Create the button
-var button = document.createElement("button");
-button.innerHTML = "Home";
+// 1. Create buttons
+var button = document.createElement("button1");
+button1.innerHTML = "Home";
+
+var button = document.createElement("button2");
 
 // 2. Append somewhere
 var body = document.getElementsByTagName("body")[0];
-body.appendChild(button);
+body.appendChild(button1);
+body.appendChild(button2);
 
-// 3. Add event handler
-button.addEventListener ("click", function() {
+// 3. Add event handlers
+button1.addEventListener ("click", function() {
   location.assign("http://nadineperez.github.io/");
 });
 
-onclick="document.getElementById('button').style.color = 'red'">
+button1.addEventListener ("click", function() {
+  changeColor();
+});
+
+function changeColor() {
+        document.getElementById('button2').style.color = 'red'
+        return false;
+} 
+
 
