ed561d14133c541dedac733e1f82ea4302bf84d3
Nadine <nadineperez96@gmail.com>
2018-02-21 17:28:50 -0500

    Update index_script.js

--- index_script.js
+++ index_script.js
@@ -1,6 +1,4 @@
-/*adding navigation menu*/
-
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='javascript_intro.html'>Javascript Introduction Assignment</a></li>" 
+"<li><a href='javascript_intro.html'>Javascript Introduction Assignment</a></li>"
 "</ul>";
