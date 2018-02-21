59f52edf699d6ca208d88d6b9347fbe7170a2732
Nadine <nadineperez96@gmail.com>
2018-02-21 15:44:02 -0500

    add btn2 functionality

--- introscript.js
+++ introscript.js
@@ -16,3 +16,12 @@ body.appendChild(btn2);
 btn.addEventListener ("click", function() {
   location.assign("http://nadineperez.github.io/");
 });
+
+
+btn2.onclick = function () {
+ var red = Math.floor(Math.random() * 256);
+ var blue = Math.floor(Math.random() * 256);
+ var green = Math.floor(Math.random() * 256);
+
+ this.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+};
