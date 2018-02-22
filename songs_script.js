60c42d1198ecf467e857087ef86203d3bd95eae2
Nadine <nadineperez96@gmail.com>
2018-02-21 19:28:25 -0500

    Create songs_script.js

---
+++ songs_script.js
@@ -0,0 +1,29 @@
+var song_1 = {name:"Good People", artist:"Jack Johnson", album:"In Between Dreams"};
+var song_2 = {name:"Super Rich Kids (feat. Early Sweatshirt)", artist:"Frank Ocean", album:"channel ORANGE"};
+var song_3 = {name:"Sitting, Waiting, Wishing", artist:"Jack Johnson", album:"In Between Dreams"};
+var song_4 = {name:"Pillow Talking (feat. Brain)", artist:"Lil Dicky", album:"Professional Rapper"};
+var song_5 = {name:"Scar Tissue", artist:"Red Hot Chili Peppers", album:"Californication"};
+
+var songs = [song_1, song_2, song_3, song_4, song_5];
+
+var body = document.getElementsByTagName("body")[0];
+  var ourTable = document.createElement("table");
+  var ourTableBody = document.createElement("tbody");
+
+  for (var i = 0; i < 5; i++) {
+    var row = document.createElement("tr");
+    for (var j = 0; j < 3; j++) {
+      var cell = document.createElement("td");
+      var cellText = songs[song_1].name;
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+    ourTableBody.appendChild(row);
+  }
+
+
+  ourTable.appendChild(ourTableBody);
+
+  body.appendChild(ourTable);
+
+  ourTable.setAttribute("border", "2");
